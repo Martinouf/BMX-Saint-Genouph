49d55d920ebb242d1c61e0d53ee88852b48aaa93
Martin Binant <martinbinant.pro@gmail.com>
2024-05-29 17:43:00 +0200

    update header

--- src/components/HeaderBmx.vue
+++ src/components/HeaderBmx.vue
@@ -1,13 +1,24 @@
 <template>
-  <header class="bg-purple-600 p-4 text-white">
-    <nav>
-      <ul class="flex space-x-4">
-        <li><router-link to="/" class="hover:underline">Accueil</router-link></li>
-        <li><router-link to="/members" class="hover:underline">Membres</router-link></li>
-        <li><router-link to="/photos" class="hover:underline">Photos</router-link></li>
-        <li><router-link to="/events" class="hover:underline">Évènements</router-link></li>
-        <li><router-link to="/contact" class="hover:underline">Contact</router-link></li>
-      </ul>
+  <header class="bg-gradient p-4 text-white">
+    <nav class="container mx-auto flex justify-between items-center px-2">
+      <!-- Logo et texte G2X à gauche -->
+      <div class="flex items-center space-x-2">
+        <img src="@/assets/logo.png" alt="Logo" class="h-8 w-8" />
+        <span class="font-bold">G2X</span>
+      </div>
+      <!-- Menu déroulant à droite -->
+      <div class="relative">
+        <button @click="toggleDropdown" class="hover:underline">Menu</button>
+        <div v-if="isDropdownOpen" class="absolute right-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg">
+          <ul>
+            <li><router-link to="/" class="block px-4 py-2 hover:bg-gray-200">Accueil</router-link></li>
+            <li><router-link to="/members" class="block px-4 py-2 hover:bg-gray-200">Membres</router-link></li>
+            <li><router-link to="/photos" class="block px-4 py-2 hover:bg-gray-200">Photos</router-link></li>
+            <li><router-link to="/events" class="block px-4 py-2 hover:bg-gray-200">Évènements</router-link></li>
+            <li><router-link to="/contact" class="block px-4 py-2 hover:bg-gray-200">Contact</router-link></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
@@ -15,5 +26,26 @@
 <script>
 export default {
   name: 'ClubHeader',
+  data() {
+    return {
+      isDropdownOpen: false,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+  },
 };
 </script>
+
+<style scoped>
+header {
+  background: linear-gradient(to bottom right, #800080, #FF69B4);
+  padding: 20px 0;
+}
+nav {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+</style>
